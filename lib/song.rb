defda33e45b94bd865fad1fec7a6dece9920c93e
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-23 19:26:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,11 +30,7 @@ class Song
   def self.genre_count 
     genre_hash = {} 
     @@genres.map do |genre|
-      if genre_hash[genre] != nil 
-        genre_hash[genre]
-  end
-end
-    genre_hash
+     
 
 end
 end
