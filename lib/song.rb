777133faae971d6665c50bac19d64c5d2415232d
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-23 19:04:18 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre
+    @@genres = []
+    @@genres << genre
+    @@count += 1 
+  end
+  
+  def self.count 
+    @@count 
+  end
+  
+  def self.genres 
+    @@genres.uniq!
+  end
+    
+end
