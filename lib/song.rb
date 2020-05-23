66872f30f7ba37130f4b7991dd7d763b089d49b5
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-23 19:24:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
     genre_hash = {} 
     @@genres.map do |genre|
       if genre_hash[genre] == nil 
-        genre_hash[genre] << genre
+        genre_hash[genre].push(genre_
   end
 end
     genre_hash
