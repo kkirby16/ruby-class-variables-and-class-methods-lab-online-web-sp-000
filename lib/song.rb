4555453402948cfa36d4ec5cc8231578fe0d529b
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-23 19:12:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   def self.genre_count 
     genre_hash = {} 
     @@genres.each do |genre|
-      if !genre_hash[genre]
+      if genre_hash[genre] == nil
         genre_hash[genre]
       else
       genre_hash[genre] += 1
